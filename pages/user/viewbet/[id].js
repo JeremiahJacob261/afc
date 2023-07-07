f2c4b5927f20ce56d0bfc08a37e3e7be9a32ac3d
Jeremiah <jeremiahjacob261@gmail.com>
2023-07-07 23:02:45 +0100

    fix fix d3c5

--- pages/user/viewbet/[id].js
+++ pages/user/viewbet/[id].js
@@ -1,81 +1,87 @@
 import { Typography, Stack, Divider, Button, Paper, TextField } from "@mui/material"
 import { supabase } from "../../api/supabase"
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Head from "next/head";
 import { useRouter } from "next/router";
-export default function Viewbets({bets}){
-const [bet,setBet] = useState(bets[0]);
-const [league,setLeague] = useState({});
+export default function Viewbets({ bets }) {
+  
+    const [league, setLeague] = useState([]);
+    const [bet,setBet] = useState({})
+    const router = useRouter();
+    const [info, setInfo] = useState({});
+    let stams;
+    let curren;
+    useEffect(() => {
+        bets.map((m) => {
+            setBet(m);
+        })
+        if (localStorage.getItem('me') === null) {
+            router.push("/login")
+        } else {
+            const GET = async () => {
+                const { data, error } = await supabase
+                    .from('users')
+                    .select()
+                    .eq('username', localStorage.getItem('me'))
+                setInfo(data[0])
+            }
+            GET();
+        }
+        const getMatchDa = async () => {
 
-const router = useRouter();
-const [info, setInfo] = useState({});
-let stams = Date.parse(bet.date + " " + bet.time) / 1000;
-let curren = new Date().getTime() / 1000
-useEffect(()=>{
-    if (localStorage.getItem('me') === null) {
-        router.push("/login")
-      }else{
-      const GET = async () => {
+            const { data, error } = await supabase
+                .from('bets')
+                .select()
+                .eq('match_id', bet.match_id);
+                data.map((damount) => {
+                    setLeague(m);
+                })
+        }
+        getMatchDa();
+         stams = Date.parse(bet.date + " " + bet.time) / 1000;
+ curren = new Date().getTime() / 1000;
+    }, [info, setLeague,stams,curren]);
+    const Depositing = async (damount, dusername) => {
         const { data, error } = await supabase
-          .from('users')
-          .select()
-          .eq('username', localStorage.getItem('me'))
-        setInfo(data[0])
-  setBalance(data[0].balance);
-      }
-      GET();
+            .rpc('depositor', { amount: damount, names: dusername })
+        console.log(error);
     }
-    const getMatchDa=async()=>{
-
-    const { data, error } = await supabase
-    .from('bets')
-    .select()
-    .eq('match_id',bet.match_id);
-    setLeague((data[0].league === 'others') ? data[0].otherl : data[0].league);
-    }
-    getMatchDa();
-},[info]);
-const Depositing = async(damount,dusername) => {
-    const { data, error } = await supabase
-    .rpc('depositor', { amount: damount, names: dusername })
-    console.log(error);
-  }
-    return(
+    return (
         <div>
-             <Head>
-        <title>Bet Details</title>
-        <meta name="description" content="Login to your Account to see whats up with your bets" />
-        <link rel="icon" href="/logo_afc.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-       </Head>
-        <Stack direction="column">
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Match Name : {bet.home} vs {bet.away}</Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>League Name: {league}</Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Stake : {bet.stake} USDT</Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Time : {bet.time}</Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Date : {bet.date} </Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>%odds : {bet.odd}%</Typography>
+            <Head>
+                <title>Bet Details</title>
+                <meta name="description" content="Login to your Account to see whats up with your bets" />
+                <link rel="icon" href="/logo_afc.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Stack direction="column">
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Match Name :{bet.home} vs {bet.away}</Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Stake : {bet.stake} USDT</Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Time : {bet.time}</Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Date : {bet.date} </Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>%odds : {bet.odd}%</Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>League Name:{league.league}</Typography>
+               
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Market Chosen : {bet.market}</Typography>
+                <Typography style={{ color: 'white', fontFamily: 'Poppins, sans-serif' }}>Potential Winnings : {bet.aim} USDT</Typography>
+                <Stack direction="row" justifyContent="space-between">
+                    <Typography direction="column" style={{ color: 'yellow', fontFamily: 'Poppins, sans-serif' }}>Results</Typography>
 
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Market Chosen : {bet.market}</Typography>
-<Typography style={{color:'white',fontFamily: 'Poppins, sans-serif'}}>Potential Winnings : {bet.aim} USDT</Typography>
-<Stack direction="row" justifyContent="space-between">
-    <Typography direction="column" style={{color:'yellow',fontFamily: 'Poppins, sans-serif'}}>Results</Typography>
-    
-<Typography style={{color:'yellow',fontFamily: 'Poppins, sans-serif',backgroundColor:'#F05D5E',padding:'5px',borderRadius:'8px',margin:'3px'}}>{(stams>curren)?'Not Started':'Processing'}</Typography>
-</Stack>
-<Button variant='standard' style={{color:'#F05D5E',display:`${(stams>curren)?'visible':'none'}`}} onClick={()=>{
-Depositing(bet.stake,info.username);
-const rem=async()=>{
+                    <Typography style={{ color: 'yellow', fontFamily: 'Poppins, sans-serif', backgroundColor: '#F05D5E', padding: '5px', borderRadius: '8px', margin: '3px' }}>{(stams > curren) ? 'Not Started' : 'Processing'}</Typography>
+                </Stack>
+                <Button variant='standard' style={{ color: '#F05D5E', display: `${(stams > curren) ? 'visible' : 'none'}` }} onClick={() => {
+                    Depositing(bet.stake, info.username);
+                    const rem = async () => {
 
-const { error } = await supabase
-  .from('placed')
-  .delete()
-  .eq('betid', bet.betid);
-}
-rem();
-  router.push('/user/bets');
-}}>Cancel this bet</Button>
-        </Stack>
+                        const { error } = await supabase
+                            .from('placed')
+                            .delete()
+                            .eq('betid', bet.betid);
+                    }
+                    rem();
+                    router.push('/user/bets');
+                }}>Cancel this bet</Button>
+            </Stack>
         </div>
     )
 }
@@ -101,7 +107,6 @@ export async function getStaticProps({ params }) {
         .select()
         .eq('betid', params.id)
     let bets = data;
-
     // Pass post data to the page via props
     return { props: { bets } }
 }
