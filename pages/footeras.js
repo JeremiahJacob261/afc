5c2182ceb36b1cbf0091cfba6452ba62e86903de
Jeremiah <jeremiahjacob261@gmail.com>
2023-08-02 14:25:30 +0100

    d14c1

--- pages/footeras.js
+++ pages/footeras.js
@@ -1,18 +1,46 @@
-import { Divider, Typography,Stack } from "@mui/material";
+import { Divider, Typography, Stack } from "@mui/material";
 import TelegramIcon from '@mui/icons-material/Telegram';
-export default function Footer(){
-    return(
-        <div style={{background:"none",width:"100%",padding:"8px"}}>
-            <Divider sx={{background:"whitesmoke"}}/>
+import gpay from '../public/simps/gpay.png'
+import usdt from '../public/simps/tether.png'
+import Image from 'next/image'
+export default function Footer() {
+    return (
+        <div style={{ background: "#1A1B72", width: "100%", padding: "8px",marginTop:'8px',borderTopLeftRadius:'8px',borderTopRightRadius:'8px' }}>
             <Stack direction="column" alignItems="center">
-        <Typography sx={{color:"#EEF0F2",fontFamily:"Nerko One, cursive",fontSize:"20px"}}>ATALANTA INVESTMENT BET</Typography>
-        <Stack direction="row">
-        <TelegramIcon sx={{color:"whitesmoke"}}/> 
-        <Typography variant="subtitle" sx={{color:"#EEF0F2",fontFamily:"Quicksand, san-serif"}}>Find US on Telegram</Typography>
-       </Stack>
-        <Typography variant="subtitle" sx={{color:"#EEF0F2",fontFamily:"Quicksand, san-serif"}}>CopyRights 2022 @ afcfifa.com</Typography>
-        <Typography variant="subtitle" sx={{color:"#EEF0F2",fontFamily:"Quicksand, san-serif"}}>Privacy</Typography>
-        <Typography variant="subtitle" sx={{color:"#EEF0F2",fontFamily:"Quicksand, san-serif"}}>Terms and Conditions</Typography>
+                <div>
+                    <Stack direction="row">
+                        <Stack direction='column'>
+                            <Typography style={{ color: '#181AA9', fontWeight: '900' ,fontFamily: "Poppins, sans-serif", fontSize: '24px' }}>
+                                AFCFIFA
+                            </Typography>
+                            <Typography  style={{width:'193px', color: '#181AA9',fontWeight: '100', fontFamily: "Poppins, sans-serif", fontSize: '10px' }}>
+                                Afc football investment is licenced by the national futures association
+                            </Typography>
+                            <Typography style={{ color: '#EE5600', fontWeight: '900' ,fontFamily: "Poppins, sans-serif", fontSize: '24px' }}>
+                                +18
+                            </Typography>
+                            <Typography  style={{width:'193px', color: '#DFA100',fontWeight: '100', fontFamily: "Poppins, sans-serif", fontSize: '10px' }}>
+                            Players must be 18 or older to register or play at AFCFIFA. Please note that gambles may have negative effects if not made with temperance.
+                            </Typography>
+                            <Typography  style={{width:'193px', color: 'white',fontWeight: '400', fontFamily: "Poppins, sans-serif", fontSize: '14px' }}>
+                         Payment Methods
+                            </Typography>
+                            <Stack direction="row" spacing={3}>
+
+                            <Image src={usdt} alt="usdt" width={22} height={20}/>
+                            <Image src={gpay} alt='gpay' width={25} height={25}/>
+                            </Stack>
+                        </Stack>
+                        <TelegramIcon sx={{ color: "whitesmoke" }} />
+                        <Typography variant="subtitle" sx={{ color: "#EEF0F2", fontFamily: "Quicksand, san-serif" }}>Find US on Telegram</Typography>
+                    </Stack>
+                </div>
+                <div>
+                    <Typography variant="subtitle" sx={{ color: "#3839C5", fontSize: '10px', fontWeight: '100', fontFamily: "Poppins, sans-serif" }}>
+                        © 2023 AfcFifa. All rights reserved.
+                    </Typography>
+
+                </div>
             </Stack>
         </div>
     )
