743531319c2a54486c8a75fb381e7cbc4f4d062e
Jeremiah <jeremiahjacob261@gmail.com>
2024-09-10 14:11:34 +0100

    fix id

--- pages/user/match/[id].js
+++ pages/user/match/[id].js
@@ -52,6 +52,82 @@ export default function Match({ matchDat }) {
     }
     useEffect(() => {
 
+        //guild functions
+    const Depositing = async (damount, dusername) => {
+        const { data, error } = await supabase
+          .rpc('depositor', { amount: damount, names: dusername })
+        console.log(error);
+      }
+  
+      const Chan = async (bets, type) => {
+        const { data, error } = await supabase
+          .rpc('chan', { bet: bets, des: type })
+        console.log(error);
+      }
+  
+      const AffBonus = async (damount, dusername, refer, lvla, lvlb) => {
+        try {
+            const { data, error } = await supabase
+                .rpc('affbonus', { name: dusername, type: 'affbonus', amount: damount, refers: refer, lvls: lvla, lvlss: lvlb })
+            console.log(error);
+        } catch (e) {
+            console.log(e)
+        }
+  
+    }
+  
+    const NUser = async (reason, username, amount) => {
+      const { error } = await supabase
+          .from('activa')
+          .insert({
+              'code': reason,
+              'username': username,
+              'amount': amount
+          });
+  }
+    //end of functions
+      const name = localStorage.getItem('signNames');
+  
+      const GETbx = async () => {
+        const { data, error } = await supabase
+          .from('placed')
+          .select('match_id,stake,aim,username,profit,market,betid')
+          .match({ username: name, won: 'null' });
+        
+  
+        //a for loop to get the match results
+        data.map(async (d) => {
+  
+          const { data: btx, error: bte } = await supabase
+            .from('bets')
+            .select('verified,results')
+            .eq('match_id', d.match_id);
+          if (btx[0].verified) {
+            try {
+              if (d.market != btx[0].results) {
+                const { data: user, error: uerror } = await supabase
+                  .from('users')
+                  .select('refer,lvla,lvlb')
+                  .eq('username', name);
+                Depositing(d.stake + d.aim, name);
+                Chan(d.betid, 'true');
+                AffBonus(parseFloat(d.profit), d.username, user.refer, user.lvla, user.lvlb);
+                NUser('bet', d.username, Number(d.aim) + Number(d.stake))
+              } else {
+                Chan(d.betid, 'false');
+              }
+              console.log('did')
+            } catch (e) {
+              console.log(e)
+            } finally {
+              window.location.reload();
+            }
+          }
+        });
+      }
+      GETbx();
+  
+
         matchDat.map((m) => {
             setMatches(m)
         })
